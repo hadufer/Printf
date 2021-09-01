d6438c355e3300993a0a605c67431737ce72e814
hadufer <dufer.hassan@gmail.com>
2021-09-01 18:37:18 +0200

    %X now truncate correctly the value returned

--- flags.c
+++ flags.c
@@ -6,7 +6,7 @@
 /*   By: hadufer <hadufer@student.42nice.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/13 19:49:28 by hadufer           #+#    #+#             */
-/*   Updated: 2021/09/01 18:27:09 by hadufer          ###   ########.fr       */
+/*   Updated: 2021/09/01 18:36:58 by hadufer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -101,7 +101,7 @@ int	specifier_handler(const char *fmt, size_t *i, t_printf *pf, va_list va_list)
 			pf->prefix = "0x";
 		if (fmt[*i] == 'X')
 			pf->capitals = 16;
-		if (fmt[*i] == 'x')
+		if (fmt[*i] == 'x' || fmt[*i] == 'X')
 			pf->truncate = 1;
 		print_unsigned(va_arg(va_list, unsigned long), 16, pf);
 	}
--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: hadufer <hadufer@student.42nice.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/09 20:16:46 by hadufer           #+#    #+#             */
-/*   Updated: 2021/09/01 18:21:36 by hadufer          ###   ########.fr       */
+/*   Updated: 2021/09/01 18:36:33 by hadufer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	main()
 {
 	char *test = malloc(sizeof(char));
 	// int	i = ;
-	printf("%x\n", test);
-	ft_printf("%x\n", test);
+	printf("%#X\n", test);
+	ft_printf("%#X\n", test);
 	return 0;
 }
